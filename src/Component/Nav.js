345617977fe263ae2df3a529b010b48a19649470
James Ngbede Samuel <115324109+Blaspace@users.noreply.github.com>
2023-10-07 01:22:08 -0800

    Update Nav.js

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -3,13 +3,13 @@ import React, { useRef } from "react";
 function Nav() {
   const navi = useRef();
 
-  /*window.addEventListener("scroll", () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY < 1) {
       navi.current.style.backgroundColor = "transparent";
     } else {
       navi.current.style.backgroundColor = "#071213";
     }
-  });*/
+  });
   return (
     <nav className="nav" ref={navi}>
       <h2>WEB</h2>
